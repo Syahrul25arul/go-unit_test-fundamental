33e4604722d3548b946e1e618a92018fabe1f829
syahrul25arul <syahrul25arul@gmail.com>
2022-07-23 21:25:38 +0700

    part-2 menggagalkan unit test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,17 +1,27 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorldSucces(t *testing.T) {
 	result := HelloWorld("Hendrik")
 	if result != "Hello Hendrik" {
-		panic("Result is not Hendrik")
+		// error
+		// t.Fail() // tidak menghentikan function unit test
+		t.Error("Result must be 'Hello Hendrik'") // akan memunculkan pesan dan memanggil function t.Fail()
 	}
+
+	fmt.Println("Test Hello World Success")
 }
 
 func TestHelloWorldFailed(t *testing.T) {
-	result := HelloWorld("Test")
+	result := HelloWorld("Hendrik")
 	if result != "Hello Hendrik" {
-		panic("Result is not Hendrik")
+		// t.FailNow() // menghentikan function unit test
+		t.Fatal("result must be 'Hello Hendrik'") // Akan memunculkan pesan dan memanggil func t.FailNow()
 	}
+
+	fmt.Println("Test Hello World Failed")
 }
